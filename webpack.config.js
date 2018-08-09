43a3f9b690488bc39a9aa61682a6160eedba180c
zzzhenng <cjlkindle01@gmail.com>
2018-08-10 07:52:44 +0800

    compress js/css

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const javascript = {
   test: /\.(js)$/,
@@ -22,21 +22,17 @@ const postcss = {
 const styles = {
   test: /\.(scss)$/,
   use: ExtractTextPlugin.extract([
-    { loader: 'css-loader', options: { sourceMap: true } },
+    { loader: 'css-loader', options: { sourceMap: true, minimize: true } },
     postcss,
     { loader: 'sass-loader', options: { sourceMap: true } },
   ]),
 };
 
-const uglify = new webpack.optimize.UglifyJsPlugin({ // eslint-disable-line
-  compress: { warnings: false },
-});
-
 const config = {
   entry: {
     App: './public/javascript/delicious-app.js',
   },
-  devtool: 'source-map',
+  // devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'public', 'dist'),
     filename: '[name].bundle.js',
@@ -47,7 +43,7 @@ const config = {
   plugins: [
     // output css to a separate file
     new ExtractTextPlugin('style.css'),
-    // new UglifyJsPlugin(),
+    new UglifyJsPlugin(),
   ],
 };
 
